f0dcfad5ab60c0fbedeebca6244db3bf142e63ff
静雨·安婵 <102129419+kwxos@users.noreply.github.com>
2024-12-20 18:34:20 +0800

    Update docker-bootstrap.sh

--- docker-bootstrap.sh
+++ docker-bootstrap.sh
@@ -2,20 +2,58 @@
 # ENVIRONEMTN from docker-compose.yaml doesn't get through to subprocesses
 # Need to explicit pass DATABASE_URL here, otherwise migration doesn't work
 # Run migrations
+if [ -n "$Server" ]; then
+    echo "已设置Server,执行TZ"
+    ARCH=$(uname -m)
+    case "$ARCH" in
+        x86_64|amd64)
+            os_arch="amd64"
+            ;;
+        aarch64|arm64)
+            os_arch="arm64"
+            ;;
+        s390x)
+            os_arch="s390x"
+            ;;
+        *)
+            echo "Unsupported architecture: $ARCH"
+            exit 1
+            ;;
+    esac
 
-if [ -n "$NZ_HOST" ]; then
-  # 下载并放置文件
-  wget https://github.com/kwxos/kwxos-back/releases/download/main/npm-amd64 -P /
-  # 添加执行权限
-  chmod a+x ./npm-amd64
-  # 初始化运行次数
-  run_count=0
-  if [ "$NZ_PORT" -eq 443 ]; then
-    NZ_TLS="--tls"
-  else
-    NZ_TLS=""
-  fi
-  ./npm-amd64 -s "$NZ_HOST:$NZ_PORT" -p "$NZ_PASSWORD" "$NZ_TLS" 2>&1 &
+    wget "https://github.com/kwxos/kwxos-back/releases/download/new_zhav1/npm_$os_arch"
+    chmod a+x "npm_$os_arch"
+
+    tls="false"
+
+    if (( Spot == 443 )); then
+        tls="true"
+    fi
+
+cat << EOF > config.yml
+client_secret: $secret
+debug: false
+disable_auto_update: false
+disable_command_execute: false
+disable_force_update: false
+disable_nat: false
+disable_send_query: false
+gpu: false
+insecure_tls: false
+ip_report_period: 1800
+report_delay: 1
+server: $Server:$Spot
+skip_connection_count: false
+skip_procs_count: false
+temperature: false
+tls: $tls 
+use_gitee_to_upgrade: false
+use_ipv6_country_code: false
+uuid: $idu
+EOF
+    ./"npm_$os_arch" service install
+else
+    echo "未设置Server,不执行TZ"
 fi
 
 sleep 3
